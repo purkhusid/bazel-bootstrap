78638cf1f75e1bdd5b5c33fc8fabb85b1f21c891
Daniel P. Purkhus <purkhusid@gmail.com>
2020-12-29 22:50:11 +0000

    Test gitpod with buildbuddy

--- WORKSPACE
+++ WORKSPACE
@@ -94,3 +94,19 @@ load("//src/go:godeps_macro.bzl", "go_repositories")
 
 # gazelle:repository_macro src/go/godeps_macro.bzl%go_repositories
 go_repositories()
+
+# Set up Remote execution toolchain using BuildBuddy
+http_archive(
+    name = "io_buildbuddy_buildbuddy_toolchain",
+    sha256 = "9055a3e6f45773cd61931eba7b7cf35d6477ab6ad8fb2f18bf9815271fc682fe",
+    strip_prefix = "buildbuddy-toolchain-52aa5d2cc6c9ba7ee4063de35987be7d1b75f8e2",
+    urls = ["https://github.com/buildbuddy-io/buildbuddy-toolchain/archive/52aa5d2cc6c9ba7ee4063de35987be7d1b75f8e2.tar.gz"],
+)
+
+load("@io_buildbuddy_buildbuddy_toolchain//:deps.bzl", "buildbuddy_deps")
+
+buildbuddy_deps()
+
+load("@io_buildbuddy_buildbuddy_toolchain//:rules.bzl", "buildbuddy")
+
+buildbuddy(name = "buildbuddy_toolchain")
---
+++ scripts/gitpod-init.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+# If the user has set added BUILDBUDDY_API_KEY to their Gitpod settings
+# we create a .bazelrc.user file which adds the needed authentication header
+# See https://www.gitpod.io/docs/environment-variables/ for information on
+# how to add environment variables to your Gitpod account
+if [[ -n "${BUILDBUDDY_API_KEY}" ]]; then
+    cat <<EOT >> .bazelrc.user
+build --remote_header=x-buildbuddy-api-key=${BUILDBUDDY_API_KEY}
+test --remote_header=x-buildbuddy-api-key=${BUILDBUDDY_API_KEY}
+query --remote_header=x-buildbuddy-api-key=${BUILDBUDDY_API_KEY}
+aquery --remote_header=x-buildbuddy-api-key=${BUILDBUDDY_API_KEY}
+cquery --remote_header=x-buildbuddy-api-key=${BUILDBUDDY_API_KEY}
+run --remote_header=x-buildbuddy-api-key=${BUILDBUDDY_API_KEY}
+EOT
+fi
